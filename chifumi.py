a34e6ae65e93db5d5760e6db1596458b71281287
Oscar Amonle <110267265+oscarduzer@users.noreply.github.com>
2023-05-14 13:56:25 +0100

    Rock,Sheet,Scissors Game

--- chifumi.py
+++ chifumi.py
@@ -2,27 +2,27 @@ from random import randint
 from threading import Thread
 def player1():
     dic={1:"Rock",2:"Sheet",3:"Scissors"}
-    valeur= input("Please make your choice:\n1-Rock\n2-Sheet\n3-Scissors\n")
-    valeur=int(valeur)
+    value= input("Please make your choice:\n1-Rock\n2-Sheet\n3-Scissors\n")
+    value=int(value)
     computer_choice=randint(1,3)
     computer_choice=int(computer_choice)
     try:
-        if valeur==computer_choice:
+        if value==computer_choice:
             print("None everywhere")
-        elif valeur==1 and computer_choice==2:
-            print(f"You chose {dic[valeur]}\nI chose Leaf\n{dic[computer_choice]}\nI Won!!!")
-        elif valeur==2 and computer_choice==1:
-            print(f"You chose {dic[valeur]}\nI chose {dic[computer_choice]}\nYou Won!!!")
-        elif valeur==1 and computer_choice==3:
-            print(f"You chose {dic[valeur]}\nI chose {dic[computer_choice]}\nYou Won!!!")
-        elif valeur==3 and computer_choice==1:
-            print(f"You chose {dic[valeur]}\nI chose Leaf\n{dic[computer_choice]}\nI Won!!!")
-        elif valeur==2 and computer_choice==3:
-            print(f"You chose {dic[valeur]}\nI chose Leaf\n{dic[computer_choice]}\nI Won!!!")
+        elif value==1 and computer_choice==2:
+            print(f"You chose {dic[value}\nI chose Leaf\n{dic[computer_choice]}\nI Won!!!")
+        elif value==2 and computer_choice==1:
+            print(f"You chose {dic[value]}\nI chose {dic[computer_choice]}\nYou Won!!!")
+        elif value==1 and computer_choice==3:
+            print(f"You chose {dic[value]}\nI chose {dic[computer_choice]}\nYou Won!!!")
+        elif value==3 and computer_choice==1:
+            print(f"You chose {dic[value]}\nI chose Leaf\n{dic[computer_choice]}\nI Won!!!")
+        elif value==2 and computer_choice==3:
+            print(f"You chose {dic[value]}\nI chose Leaf\n{dic[computer_choice]}\nI Won!!!")
         else:
-            print(f"You chose {dic[valeur]}\nI chose {dic[computer_choice]}\nYou Won!!!")
+            print(f"You chose {dic[value]}\nI chose {dic[computer_choice]}\nYou Won!!!")
     except KeyError:
-        print("Erreur de choix")
+        print("Choice's Error")
 thread1=Thread(target=player1)
 
 thread1.start()
